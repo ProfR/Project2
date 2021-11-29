c3785de29e168d4960e1aee54bbd3e8dadbc3aee
ProfR <msrvks@rit.edu>
2021-11-28 19:19:46 -0500

    Current version -- input is all but toppings and meats

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -11,6 +11,11 @@ namespace Project2 {
 		private static bool isDelivery;
 		const bool SAMELINE = false;
 		static string customerName;
+		static Size size;
+		static Dressing dressing;
+		static Cheese cheese;
+		static Topping[] toppings;
+		static Meat[] meats;
 
 		static void Main( string[] args ) {
 			Welcome();
@@ -39,9 +44,10 @@ namespace Project2 {
 			Utility.WriteColor( "Is this a delivery order? (y/n) ==> ", Utility.PROMPT, SAMELINE );
 			char deliveryTest = Console.ReadLine()[0];
 			if ( char.ToUpper(deliveryTest) != 'Y') {
-				Utility.WriteColor( "  ==> Invalid Entry -- Order set as pickup ", Utility.ERROR );
+				Utility.WriteColor( "  ==> Order set as pickup ", Utility.MENU );
 				isDelivery = false;
 			} else {
+				Utility.WriteColor( "  ==> Order set as delivery ", Utility.MENU );
 				isDelivery = true;
 			}
 
@@ -49,7 +55,6 @@ namespace Project2 {
 
 		public static void TakeOrder() {
 			int sandwichCount = 0;
-
 
 			do {
 				sandwichCount++;
@@ -63,15 +68,41 @@ namespace Project2 {
 		}
 
 		public static void GetSandwich ( int _count ) {
-			Utility.WriteColor( $" ==> Now entering selections for Sandwich #{_count}\n", Utility.PROMPT );
+			Utility.WriteColor( $"\n ==> Now entering selections for Sandwich #{_count}\n", Utility.PROMPT );
 
+			size = (Size)GetSandwichSize();
+			dressing = (Dressing)GetDressing();
+
+			cheese = (Cheese)GetCheese();
+
+
+			Console.WriteLine( $"{size}" );
 			Sandwich aSandwich = new Sandwich();
 			sandwiches.Add( aSandwich );
-			
+		}
+
+		public static int GetSandwichSize() {
+			List<string> sizes = new List<string>();
+			foreach ( Size value in Enum.GetValues( typeof( Size ) ) ) sizes.Add( value.ToString() );
+			return Utility.GetEnumValue( sizes, "\nSelect a sandwich size: ", "Enter the selection for the desired sandwich size: " );
+
+		}
+
+		public static int GetDressing() {
+			List<string> dressings = new List<string>();
+			foreach ( Dressing value in Enum.GetValues( typeof( Dressing ) ) ) dressings.Add( value.ToString() );
+			return Utility.GetEnumValue( dressings, "\nSelect a dressing: ", "Enter the selection for the desired dressing: " );
+		}
+
+		public static int GetCheese() {
+			List<string> cheeses = new List<string>();
+			foreach ( Cheese value in Enum.GetValues( typeof( Cheese ) ) ) cheeses.Add( value.ToString() );
+			return Utility.GetEnumValue( cheeses, "\nSelect a cheese: ", "Enter the selection for the desired cheese: " );
 		}
 
 		public static void CreateReceipt() {
 			Console.WriteLine( "DONE" );
 		}
+
 	}
 }
